e83814c334296197385bf0dd3cb2fbc9c0ce0312
andreasdam2017 <andreasdam2017@gmail.com>
2025-12-04 12:47:34 +0000

    .

--- Show_sales_inventory.py
+++ Show_sales_inventory.py
@@ -1,25 +1,32 @@
 import json
 import csv
 import os
+import reader
+
 #We make the working directory into the file directory
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath) 
 os.chdir(dname)
 
-with open("inventory.json", "r") as f:
-    inventory_categories = json.load(f)
 with open("sales.json", "r") as f:
-    sales_dict = json.load(f)
+    sales_list = json.load(f)
+inventory_categories = reader.reader_system('inventory.json')
 #print it out in a readable way
-print("Inventory Categories:")
-print("-" * 100)    
-for category, items in inventory_categories.items():    
-    print(f"\nCategory: {category}")
-     
-    for item in items:
-        print(f"  \nItem ID: {item['item_id']} \nName: {item['item_name']} \nPrice: {item['price']} \nQuantity in Stock: {item['quantity_in_stock']}")
-    print("-" * 100)     
-    
+
+def display(display_file):
+    #This functions accepts dictionaries and displays the content of said dictionary in a readable way. It does not return any
+    #Value, but just displays the content.
+    display_items = reader.reader_system(display_file)
+    for cate_name, items in display_items.items():#We take the outer dict and display its name in a for loop
+        print()
+        print("-" * 40)
+        print(f"Category: {cate_name}")     
+        for index, item in enumerate(items, start=1):#We make an enumerate in the inner dict in the nested loop so that we can,
+            print(f"  Item {index}:")                #find its index in the list
+            for key, value in item.items():          #We print the key and values in the index
+                print(f"    {key} = {value}")
+                
+display('inventory.json')#Change name if you wish to see inventory.json or sales.json file
 
     
 
