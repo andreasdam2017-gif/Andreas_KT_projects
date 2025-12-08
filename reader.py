1bd195f66349459517cc3ac7a66572f89b09242d
Frystiboks <Frystiboks@protonmail.com>
2025-12-08 11:50:10 +0000

    fixed reader and started on backup swap

--- reader.py
+++ reader.py
@@ -21,11 +21,15 @@ def reader_userinput(directory):
         loaded_file = {'':[]}
     return loaded_file
 
+
 def reader_system(file_name):
-    if file_name.endswith('.json'):
-        with open(file_name, 'r') as file:
-            loaded_file = json.load(file)
-        return loaded_file
-    elif file_name.endswith('.txt'):
-        with open(file_name, 'r') as file:
-            return file
+    try:
+        if file_name.endswith('.json'):
+            with open(file_name, 'r') as file:
+                loaded_file = json.load(file)
+            return loaded_file
+        elif file_name.endswith('.txt'):
+            with open(file_name, 'r') as file:
+                return file.readlines()
+    except:
+        return {}
